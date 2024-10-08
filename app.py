7d4916d028712439c57d9ac53e7db2182b580918
sumitjha33 <sumitjha3333@gmail.com>
2024-10-08 16:50:57 +0530

    Final chnages

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from timezonefinder import TimezoneFinder
 from datetime import datetime
 import requests
 import pytz
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
